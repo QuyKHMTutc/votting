a6af815ab57c9b3764a2b1463288f6e70767b92d
guyguy <quykhmtutc@gmail.com>
2026-02-07 16:44:17 +0700

    first commit

---
+++ src/main/java/com/xxxx/votting_spring/VottingSpringApplication.java
@@ -0,0 +1,13 @@
+package com.xxxx.votting_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VottingSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VottingSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xxxx/votting_spring/VottingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxx.votting_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VottingSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
